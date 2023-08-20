5ba4941bb82e37b1e30d9c76ea422824839d9d3f
mdmunnaahmed <msdmunna77@gmail.com>
2023-08-20 22:59:31 +0600

    my sass folder  to vue  --added

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import '@/assets/main.scss'
-// import "bootstrap";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import router from './router.js'
+import router from "./router.js";
 import store from "./store/index.js";
 
-const app = createApp(App)
-app.use(router)
-app.use(store)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount("#app");
